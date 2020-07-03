998f6d5d9bfd8c658209ab27e377d2905b1f9974
Josh Frechette <josh.f.design@gmail.com>
2020-07-03 13:05:16 -0400

    links model.

--- models/links.js
+++ models/links.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const launchPastSchema = new Schema({
+const linksSchema = new Schema({
    
-          mission_name: String,
-          launch_date_local: String,
-          // launch_site 
-          // links 
-          // rocket 
-          // ships 
+          article_link: String,
+          video_link: String
+
 });
 
-module.exports = mongoose.model('launchesPast', launchPastSchema)
+module.exports = mongoose.model('links', linksSchema)
